735381ac43b1182b85b2721530519596e573f905
Your NameYour <kevin.guevara090@gmail.com>
2022-09-05 15:40:52 -0500

    se corrigio un error al agregar y verificar nuevas palabras

--- code.js
+++ code.js
@@ -1,82 +1,96 @@
 const boton = document.querySelector("#startGame");
 const input = document.querySelector("#input-letters");
 const lineas = document.querySelector("#lineas");
-
-const listaPalabras = ["uno","dos","tres","cuatro","cinco","elefante","hipopotamo"];
-const aleatorio = Math.round(Math.random()*(listaPalabras.length-1));
-const palabra=[listaPalabras[aleatorio]];
-
-cantidadDeLineas ="";
-for (i=0;i<palabra[0].length;i++){
-    cantidadDeLineas += `<div style="display:block  ;margin:auto;">
-                            <p class="letraAdivinar item${i+1}" style="color: white;">${palabra[0][i]}</p>
-                            <img id="lineas-img" src="../images/Rectangulo.png" alt="">
-                        </div>`
-};
-lineas.insertAdjacentHTML(`afterbegin`,cantidadDeLineas);
-
-var intento = 1 ;
-var verificador= 0;
-var continuar = true;
-letraAdivinada="";
-const verificarCaracteres =(letras)=>{
-    for (j=0;j<palabra[0].length;j++){
-        // console.log(`letra en funcion => ${letras}`)
-        if (letras == palabra[0][j]){
-            console.log(`SI esta la letra ${letras}`);
-            document.querySelector(`.item${j+1}`).style.color="black";
-            letraAdivinada +=letras;
-            verificador++
-        }
-    }
-} 
-
-const Verificar =(letra)=>{
-    const ahorcado = document.querySelector("#ahorcado");
-    if (palabra[0].includes(letra)&&(letraAdivinada.includes(letra)==false)){
-        verificarCaracteres(letra);
-        if (verificador>=palabra[0].length){
-            document.querySelector("#input-letters").readOnly=true;
-            mensaje("Ganaste")
-        }
-    }else{
-        intento++
-        console.log(`NO esta la letra ${letra}`);
-        if (intento<9){
-            ahorcado.src=`../images/img${intento}.png`;
-        }else if (intento==9){
-            ahorcado.src=`../images/img${intento}.png`;
-            document.querySelector("#input-letters").readOnly=true;
-            mensaje("Perdiste")
-        }
-    }
+const ahorcado = document.querySelector("#ahorcado");
+let palabra = null ;
+var listaPalabras = ["uno","dos","tres","cuatro","cinco","elefante","hipopotamo"];
+function listado (){
+    const aleatorio = Math.round(Math.random()*(listaPalabras.length-1));
+    palabra=[listaPalabras[aleatorio]];
 }
-const mensaje =(accion)=>{
-    continuar = false;
-    if (accion=="Perdiste"){
-        let errores = document.querySelector("#error").style.display="flex";
-        setTimeout(function(){
-            errores =  document.querySelector("#error").style.display="none";
-        },3000);
-    }
-    else if (accion=="Ganaste"){
-        let correcto = document.querySelector("#correcto").style.display="flex";
-        setTimeout(function(){
-            correcto =  document.querySelector("#correcto").style.display="none";
-        },3000);
-    }
-}
-let letrasExcritas="";
-input.addEventListener("keydown",(e)=>{
-    let letra = e.key;
-    if (letra.charCodeAt()>96&&letra.charCodeAt()<123||letra.charCodeAt()==46||letra.charCodeAt()==10||letra.charCodeAt()==32||letra.charCodeAt()==44||letra.charCodeAt()>47&&letra.charCodeAt()<58) {
-        if(continuar && (letrasExcritas.includes(letra)==false)){
-            letrasExcritas += letra;
-            Verificar(letra)
-        }
-    }; 
-})
+juego()
+function juego (){
+    listado()
+    ahorcado.src=`../images/img${1}.png`;
+    input.value=""
+    lineas.innerHTML="";
+    let intento = 1 ;
+    let verificador= 0;
+    let continuar = true;
+    let letraAdivinada="";
 
+    cantidadDeLineas ="";
+    for (i=0;i<palabra[0].length;i++){
+        cantidadDeLineas += `<div style="display:block  ;margin:auto;">
+                                <p class="letraAdivinar item${i+1}" style="color: white;">${palabra[0][i]}</p>
+                                <img id="lineas-img" src="../images/Rectangulo.png" alt="">
+                            </div>`
+    };
+
+    lineas.insertAdjacentHTML(`afterbegin`,cantidadDeLineas);
+    cantidadDeLineas ="";
+
+
+    const verificarCaracteres =(letras)=>{
+        for (j=0;j<palabra[0].length;j++){
+            console.log(`letra => ${letras} // palabra => ${palabra[0][j]}`)
+            // console.log(`letra en funcion => ${letras}`)
+            if (letras == palabra[0][j]){
+                console.log(`SI esta la letra ${letras}`);
+                document.querySelector(`.item${j+1}`).style.color="black";
+                letraAdivinada +=letras;
+                verificador++
+            }
+        }
+    } 
+
+    const Verificar =(letra)=>{
+        console.log(`<b>${palabra}</b>`)
+        if (palabra[0].includes(letra)&&(letraAdivinada.includes(letra)==false)){
+            console.log(letra)
+            verificarCaracteres(letra);
+            if (verificador>=palabra[0].length){
+                document.querySelector("#input-letters").readOnly=true;
+                mensaje("Ganaste")
+            }
+        }else{
+            intento++
+            console.log(`NO esta la letra ${letra} <br> ${palabra[0].includes(letra)}`);
+            if (intento<9){
+                ahorcado.src=`../images/img${intento}.png`;
+            }else if (intento==9){
+                ahorcado.src=`../images/img${intento}.png`;
+                document.querySelector("#input-letters").readOnly=true;
+                mensaje("Perdiste")
+            }
+        }
+    }
+    const mensaje =(accion)=>{
+        continuar = false;
+        if (accion=="Perdiste"){
+            let errores = document.querySelector("#error").style.display="flex";
+            setTimeout(function(){
+                errores =  document.querySelector("#error").style.display="none";
+            },3000);
+        }
+        else if (accion=="Ganaste"){
+            let correcto = document.querySelector("#correcto").style.display="flex";
+            setTimeout(function(){
+                correcto =  document.querySelector("#correcto").style.display="none";
+            },3000);
+        }
+    }
+    let letrasExcritas="";
+    input.addEventListener("keydown",(e)=>{
+        let letra = e.key;
+        if (letra.charCodeAt()>96&&letra.charCodeAt()<123||letra.charCodeAt()==46||letra.charCodeAt()==10||letra.charCodeAt()==32||letra.charCodeAt()==44||letra.charCodeAt()>47&&letra.charCodeAt()<58) {
+            if(continuar && (letrasExcritas.includes(letra)==false)){
+                letrasExcritas += letra;
+                Verificar(letra)
+            }
+        }; 
+    })
+}
 const boton_addWords = document.querySelector("#boton-addWords");
 const agregarPalabra = document.querySelector(".agregarPalabra");
 const GuardarPalabra = document.querySelector("#GuardarPalabra");
@@ -90,6 +104,7 @@ GuardarPalabra.addEventListener("click",()=>{
     if (nuevaPalabra.value != ""){
         listaPalabras.push(`${nuevaPalabra}`);
         agregarPalabra.value="";
+        juego()
         document.querySelector(".container-addWord").style.display="none" 
     }
 })
